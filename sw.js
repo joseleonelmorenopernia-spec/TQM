67ffdf51ac287821637089c3de83284add5099fc
JLMP <joseleonelmorenopernia@gmail.com>
2026-01-02 16:32:51 -0400

    feat: Enhance PWA capabilities and cache assets
    Improves the Progressive Web App experience by enabling standalone mode and updating manifest configurations for better installation prompts. Updates cache strategy in the service worker to include essential assets and a new cache version.

--- sw.js
+++ sw.js
@@ -1,16 +1,28 @@
 
-const CACHE_NAME = 'love-app-v1';
+const CACHE_NAME = 'love-app-v2';
 const ASSETS = [
   './',
   './index.html',
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://img.icons8.com/emoji/512/orange-heart.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
 });
